07e1eb250783177c66b33c85e034be6eabbb440f
afanfian <afanfian14@gmail.com>
2023-05-22 08:30:46 +0700

    feat: refactor code in class TimeServer server.py

--- threading_examples/server.py
+++ threading_examples/server.py
@@ -5,14 +5,14 @@ import time
 
 class ClientProcessor(threading.Thread):
     def __init__(self, connection, address, server):
-        self.connection = connection
-        self.address = address
-        self.server = server
+        self.connection = connection  # Koneksi socket dengan klien
+        self.address = address  # Alamat klien
+        self.server = server  # Referensi ke objek TimeServer
         threading.Thread.__init__(self)
 
     def run(self):
         while True:
-            data = self.connection.recv(32)
+            data = self.connection.recv(32)  # Menerima data dari klien
             if not data:
                 break
             logging.warning(f"[CLIENT PROCESSOR] received {data} from {self.address}")
@@ -29,43 +29,44 @@ class ClientProcessor(threading.Thread):
             else:
                 break
 
-        self.connection.close()
+        self.connection.close()  # Menutup koneksi dengan klien
 
 class TimeServer(threading.Thread):
     def __init__(self):
         self.clients = []  # Daftar klien yang terhubung
-        self.client_count = 0  # Jumlah klien yang terhubung
         self.response_count = 0  # Jumlah response yang dikirimkan
         threading.Thread.__init__(self)
 
     def run(self):
+        # Membuka socket server
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            my_socket.bind(('0.0.0.0', 45000))  # Menghubungkan socket ke alamat dan port tertentu
-            my_socket.listen(1)  # Melihat koneksi masuk dengan backlog 1
+            my_socket.bind(('0.0.0.0', 45000))  # Mengikat socket ke alamat dan port tertentu
+            my_socket.listen(1)  # Menerima koneksi masuk dengan backlog 1
+            logging.warning("Server dimulai menghubungkan ke koneksi...")
             while True:
                 connection, client_address = my_socket.accept()  # Menerima koneksi baru
-                logging.warning(f"Koneksi baru dari{client_address}")
-
-                client_processor = ClientProcessor(connection, client_address, self)  # Membuat instance ClientProcessor untuk setiap koneksi
+                logging.warning(f"Koneksi baru dari {client_address}")
+                # Membuat instance ClientProcessor untuk setiap koneksi
+                client_processor = ClientProcessor(connection, client_address, self)
                 client_processor.start()  # Memulai thread ClientProcessor
                 self.clients.append(client_processor)  # Menambahkan ClientProcessor ke daftar klien yang terhubung
-                self.client_count += 1  # Menambahkan jumlah klien yang terhubung
-                logging.warning(f"Total pengguna yang terhubung: {self.client_count}")
 
     def update_client_count(self):
         self.response_count += 1  # Menambahkan jumlah response yang dikirimkan
-        logging.warning(f"Total response pesan: {self.response_count}")
+        logging.warning(f"Total pesan respons: {self.response_count}")
 
     def stop(self):
-        # Tambahkan log di sini jika diperlukan sebelum menutup socket
-        self.my_socket.close()
+        for client in self.clients:
+            client.join()  # Menunggu semua thread ClientProcessor selesai
+        logging.warning("Menghentikan server")
 
 def main():
     logging.basicConfig(level=logging.WARNING)
     server = TimeServer()
     server.start()
-    server.join()
+    time.sleep(60)  # Menjalankan server selama 1 menit
+    server.stop()  # Menghentikan server setelah 1 menit
 
 if __name__ == "__main__":
     main()
