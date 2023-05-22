b29bc5ad64fdbe0a7ac07f203a82cbbc782dc3e9
afanfian <afanfian14@gmail.com>
2023-05-22 20:04:03 +0700

    feat: add coment clientProcess

--- threading_examples/clientProcess.py
+++ threading_examples/clientProcess.py
@@ -4,12 +4,13 @@ import multiprocessing
 import time
 
 def send_time_request():
+    # Terhubung ke server dan mengirim permintaan waktu
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         server_address = ('localhost', 45000)
-        logging.warning(f"Opening socket {server_address}")
+        logging.warning(f"Membuka soket {server_address}")
         sock.connect(server_address)
         message = 'TIME\r\n'
-        logging.warning(f"[CLIENT] Sending {message}")
+        logging.warning(f"[CLIENT] Mengirim {message}")
         sock.sendall(message.encode('utf-8'))
         data = sock.recv(16)
         logging.warning(f"[DITERIMA DARI SERVER] {data}")
@@ -20,9 +21,9 @@ if __name__ == '__main__':
     process_count = 0  # Counter untuk jumlah proses yang dibuat
 
     while time.time() - start_time < duration:
-        p = multiprocessing.Process(target=send_time_request)
-        p.start()
-        p.join()
-        process_count += 1
+        p = multiprocessing.Process(target=send_time_request)  # Membuat proses baru untuk setiap permintaan
+        p.start()  # Memulai proses
+        p.join()  # Menunggu proses selesai
+        process_count += 1  # Menambahkan jumlah proses
 
-    logging.warning(f"Total pesan request: {process_count}")
+    logging.warning(f"Total pesan request: {process_count}")  # Mencatat jumlah total permintaan
